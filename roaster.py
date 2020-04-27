e4a9927fd34c7f11a6b47fee8390f30e71111e33
Timothy A. DeWees <timothy.dewees@gmail.com>
2020-04-27 12:56:27 +0000

    Motor controller work

--- roaster.py
+++ roaster.py
@@ -1,8 +1,7 @@
 from utils import HeatingElementController, MotorController, ThermocoupleController, DisplayPanelController
 
 HEATING_ELEMENT_CONTROL_PIN = 12
-MOTOR_CONTROL_PIN = 14
-DISPLAY_I2CBUS = 0
+DISPLAY_I2CBUS = 1
 DISPLAY_ADDRESS = 0x27
 
 
@@ -11,16 +10,14 @@ def run_roaster():
     heating.power_on()
     heating.power_off()
 
-    motor = MotorController(MOTOR_CONTROL_PIN)
+    motor = MotorController()
     motor.drive_motor("r", "h")
 
     therm = ThermocoupleController()
     print(therm.read_temp_c())
     print(therm.read_temp_f())
 
-    #display = DisplayPanelController(DISPLAY_I2CBUS, DISPLAY_ADDRESS)
-
-
+    display = DisplayPanelController(DISPLAY_I2CBUS, DISPLAY_ADDRESS)
 
 
 if __name__ == "__main__":
--- utils/motor_controller.py
+++ utils/motor_controller.py
@@ -1,9 +1,32 @@
+import RPi.GPIO as GPIO
+
 class MotorController:
 
-    MOTOR_CONTROL_PIN = 0
+    MOTOR_FORWARD = 17
+    MOTOR_REVERSE = 18
 
-    def __init__(self, control_pin):
-        self.MOTOR_CONTROL_PIN = control_pin
+    def __init__(self):
+        
+        GPIO.cleanup()
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.MOTOR_FORWARD, GPIO.OUT)
+        GPIO.setup(self.MOTOR_REVERSE, GPIO.OUT)
 
     def drive_motor(self, direction='f', speed='m'):
-        print("Driving motor {0} at speed {1}".format(direction, speed))
+        
+        self.stop_motor()
+
+        if direction == 'f':
+            print("MOTOR FORWARD");
+            GPIO.outout(self.MOTOR_FORWARD, GPIO.HIGH)
+        elif direction == 'r':
+            print("MOTOR REVERSE");
+            GPIO.output(self.MOTOR_REVERSE, GPIO.HIGH)
+
+
+    def stop_motor(self):
+        
+        print("MOTOR STOP")
+        GPIO.output(self.MOTOR_FORWARD, GPIO.LOW) 
+        GPIO.output(self.MOTOR_REVERSE, GPIO.LOW) 
