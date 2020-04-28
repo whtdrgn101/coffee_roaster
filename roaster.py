17550203275bfcfeed75d6fe7f69f7e12a72dd24
Timothy A. DeWees <timothy.dewees@gmail.com>
2020-04-28 19:29:06 -0400

    testing

--- roaster.py
+++ roaster.py
@@ -2,74 +2,87 @@ from utils import RoasterConfig, HeatingElementController, MotorController, Ther
 import RPi.GPIO as GPIO
 import time
 
-def run_roaster():
+class RoastMaster:
 
-    RUNNING = True
+    RUNNING = False 
+    config = None 
+    start_button = None
+    stop_button = None
+    heating = None
+    drive_motor = None
+    therm = None
 
-    def handle_stop_press():
-        RUNNING = False
+    def __init__(self):
+        
+        # All GPIO commands will be done by PIN number
+        GPIO.setmode(GPIO.BOARD)
+        print(GPIO.getmode())
+        #This will blow up until the device is actually hooked up
+        #display = DisplayPanelController(config.LCD_BUS_NUMBER, config.LCD_ADDRESS)
 
-    def handle_start_press():
-        RUNNING = True 
+        # Bring up roaster components
+        print("Loading Config")
+        self.config = RoasterConfig()
 
-    # All GPIO commands will be done by PIN number
-    GPIO.setmode(GPIO.BCM)
+        # Setup Roaster Components
+        self.start_button = ButtonController(self.config.START_BUTTON_PIN, self.handle_start_press)
+        self.stop_button = ButtonController(self.config.STOP_BUTTON_PIN, self.handle_stop_press)
+        self.heating = HeatingElementController(self.config.HEATING_ELEMENT_PIN)
+        self.drive_motor = MotorController("DRIVE", self.config.MOTOR_FORWARD_PIN, self.config.MOTOR_REVERSE_PIN)
+        self.blower_motor = MotorController("BLOWER", self.config.BLOWER_FORWARD_PIN, self.config.BLOWER_REVERSE_PIN)
+        self.therm = ThermocoupleController()
 
-    #This will blow up until the device is actually hooked up
-    #display = DisplayPanelController(config.LCD_BUS_NUMBER, config.LCD_ADDRESS)
 
-    # Bring up roaster components
-    print("Loading Config")
-    config = RoasterConfig()
+    def handle_stop_press(self):
+        print("STOP PRESS")
+        self.RUNNING = False
 
-    # Setup Roaster Components
-    start_button = ButtonController(config.START_BUTTON_PIN, handle_start_press)
-    stop_button = ButtonController(config.STOP_BUTTON_PIN, handle_stop_press)
-    heating = HeatingElementController(config.HEATING_ELEMENT_PIN)
-    drive_motor = MotorController("DRIVE", config.MOTOR_FORWARD_PIN, config.MOTOR_REVERSE_PIN)
-    blower_motor = MotorController("BLOWER", config.BLOWER_FORWARD_PIN, config.BLOWER_REVERSE_PIN)
-    therm = ThermocoupleController()
+    def handle_start_press(self):
+        print("START PRESS")
+        self.RUNNING = True 
 
-    try:
+    def run_roaster(self):
 
-        while True:
+        try:
 
-            if RUNNING == True:
+            while True:
 
-                # TEMP & HEATING STATE
-                temp = therm.read_temp_f()
+                if self.RUNNING == True:
 
-                if temp > config.ROAST_TEMP and heating.IS_ON == True:
-                    heating.power_off()
-                elif temp < config.ROAST_TEMP and heating.IS_ON == False:
-                    heating.power_on()
+                    # TEMP & HEATING STATE
+                    temp = self.therm.read_temp_f()
 
-                # MOTOR STATE
-                if drive_motor.IS_MOVING == False:
-                    drive_motor.drive_motor("f")
-                if blower_motor.IS_MOVING == False:
-                    blower_motor.drive_motor("f")
+                    if temp > self.config.ROAST_TEMP and self.heating.IS_ON == True:
+                        self.heating.power_off()
+                    elif temp < self.config.ROAST_TEMP and self.heating.IS_ON == False:
+                        self.heating.power_on()
 
-                # Display Status
-                print("Temp: {0}c {1}f".format(therm.read_temp_c(), therm.read_temp_f()))
-                time.sleep(1)
+                    # MOTOR STATE
+                    if self.drive_motor.IS_MOVING == False:
+                        self.drive_motor.drive_motor("f")
+                    if self.blower_motor.IS_MOVING == False:
+                        self.blower_motor.drive_motor("f")
 
-            else:
-                print("Press Start Button")
-                time.sleep(1)
+                    # Display Status
+                    print("Temp: {0}f".format(temp))
+                    time.sleep(1)
+
+                else:
+                    print("Press Start Button")
+                    time.sleep(1)
 
     
-    except KeyboardInterrupt:
-        print("Shutting down")
-        heating.power_off()
-        drive_motor.stop_motor()
-        blower_motor.stop_motor()
+        except KeyboardInterrupt:
+            print("Shutting down")
+            self.heating.power_off()
+            self.drive_motor.stop_motor()
+            self.blower_motor.stop_motor()
 
-    finally:
-        RUNNING = False
-        GPIO.cleanup()
+        finally:
+            self.RUNNING = False
 
 if __name__ == "__main__":
     # Run the main loop for the roaster
-    RUNNING = False
-    run_roaster()
+    roaster = RoastMaster()
+    roaster.run_roaster()
+    GPIO.cleanup()
--- utils/button_controller.py
+++ utils/button_controller.py
@@ -2,6 +2,6 @@ import RPi.GPIO as GPIO
 
 class ButtonController:
 
-    def __init__(self, pin, callback):
-        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.add_event_detect(pin, GPIO.FALLING, callback=callback, bouncetime=300)
+    def __init__(self, pin, cbk):
+        GPIO.setup(pin, GPIO.IN)#, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(pin, GPIO.RISING, callback=lambda x: cbk(), bouncetime=300)
