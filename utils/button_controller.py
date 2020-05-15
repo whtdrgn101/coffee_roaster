92445f3ce5d0625633317e47745f34c5407e0706
Timothy A. DeWees <timothy.dewees@gmail.com>
2020-05-15 13:14:53 -0400

    setting bounce detection to 2 seconds on buttons

--- utils/button_controller.py
+++ utils/button_controller.py
@@ -4,4 +4,4 @@ class ButtonController:
 
     def __init__(self, pin, cbk):
         GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(pin, GPIO.RISING, callback=lambda x: cbk(), bouncetime=500)
+        GPIO.add_event_detect(pin, GPIO.RISING, callback=lambda x: cbk(), bouncetime=2000)
