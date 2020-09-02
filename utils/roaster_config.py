f81536fcf320ed2ebdc4358abc1f6de3fbbdb5ee
Timothy DeWees <whtdrgn101@users.noreply.github.com>
2020-09-02 15:59:11 -0400

    Update roaster_config.py
    Switching motor to L298 driver with PWM speed control

--- utils/roaster_config.py
+++ utils/roaster_config.py
@@ -2,7 +2,10 @@ import json
 
 class RoasterConfig:
 
-    MOTOR_RELAY_PIN = 0
+    MOTOR_FORWARD_PIN = 0
+	MOTOR_REVERSE_PIN = 0
+	MOTOR_PWM_PIN = 0
+	MOTOR_SPEED = 0.5
     BLOWER_RELAY_PIN = 0
     HEATING_ELEMENT_PIN = 0
     START_BUTTON_PIN = 0
@@ -18,7 +21,10 @@ class RoasterConfig:
             config = json.load(f)
 
         #parse file
-        self.MOTOR_RELAY_PIN = config["MOTOR_RELAY_PIN"]
+        self.MOTOR_FORWARD_PIN = config["MOTOR_FORWARD_PIN"]
+        self.MOTOR_REVERSE_PIN = config["MOTOR_REVERSE_PIN"]
+        self.MOTOR_PWM_PIN = config["MOTOR_PWM_PIN"]
+        self.MOTOR_SPEED = config["MOTOR_SPEED"]
         self.BLOWER_RELAY_PIN = config["BLOWER_RELAY_PIN"]
         self.HEATING_ELEMENT_PIN = config["HEATING_ELEMENT_PIN"]
         self.START_BUTTON_PIN = config["START_BUTTON_PIN"]
